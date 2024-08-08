1fe3fa4bac498d801ead3f6167ebe0c415d70c47
Tinaguhwa23 <tinaguhwa@gmail.com>
2024-08-08 14:27:15 -0700

    my commits

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,24 +1,16 @@
-def pascal_triangle(n):
+#!/usr/bin/python3
+"""
+0-main
+"""
+pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
+
+def print_triangle(triangle):
         """
-            Returns a list of lists representing Pascal's Triangle up to the nth row.
+            Print the triangle
                 """
-                    if n <= 0:
-                                return []
+                    for row in triangle:
+                                print("[{}]".format(",".join([str(x) for x in row])))
 
-                                triangle = []  # Initialize an empty list to hold all rows of the triangle
 
-                                    for i in range(n):
-                                                # Start each row with 1
-                                                        row = [1] * (i + 1)
-                                                                
-                                                                        # Fill in the interior values of the row (if i > 1)
-                                                                                if i > 1:
-                                                                                                for j in range(1, i):
-                                                                                                                    # Each element is the sum of the two elements directly above it
-                                                                                                                                    row[j] = triangle[i-1][j-1] + triangle[i-1][j]
-
-                                                                                                                                            # Append the completed row to the triangle
-                                                                                                                                                    triangle.append(row)
-
-                                                                                                                                                        return triangle
-
+                                if __name__ == "__main__":
+                                        print_triangle(pascal_triangle(0))
