fc6b3f90a79693e65171753f075301a9e8d27e0a
Tinaguhwa23 <tinaguhwa@gmail.com>
2024-08-08 14:13:58 -0700

    my commit

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+0-pascal_triangle
+"""
+
+
+def pascal_triangle(n):
+        """
+                Create rows for pascal triangle
+                    """
+                        triangle = []
+
+                            if n <= 0:
+                                        return triangle
+
+                                        for i in range(n):
+                                                    row = [1] * (i + 1)
+                                                            for j in range(1, i):
+                                                                            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+                                                                                    triangle.append(row)
+
+                                                                                        return triangle
